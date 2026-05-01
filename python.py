23205f7667a3c78999c1890bdbd7c11da005511f
raayiii <raayiii18@gmail.com>
2026-05-01 23:24:33 +0700

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 abc0 = "123"
-v_abc = [4, 5, 6, 7]
+abc = [4, 5, 6, 7]
 print(123 + int(abc0))
-for i in range(len(v_abc)):
-    temp = v_abc[i]
+for i in range(4):
+    temp = abc[i]
     print(temp)
